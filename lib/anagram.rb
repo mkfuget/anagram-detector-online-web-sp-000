cc6e6cc3c9161b744a83930437aa0722862e5350
Michael Fuget <michaelfuget@gmail.com>
2020-12-03 01:53:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,8 +2,36 @@
 class Anagram
   attr_accessor :word 
   
-  def self.word_to_letter_counts_object(word)
+  def self.word_to_letter_counts_map(word)
     out = {}
-    
+    word.split('').each do |letter|
+      if(!out.has_key?(letter))
+        out[letter] = 1;
+      else 
+        out[letter]+=1;
+      end 
+    end 
+  end 
+  
+  def match(array)
+    word_char_counts = Anagram.word_to_letter_counts_map(@word)
+    out = []
+    array.each do |array_word|
+      is_match? = true
+      array_word_char_counts = Anagram.word_to_letter_counts_map(array_word)
+      word_char_counts.each |key, value|
+        if(array_word_char_counts[key] != value)
+          match = false 
+        end 
+      end 
+      
+      if(array_word.length != @word.length)
+        match = false 
+      end 
+      
+      if(match)
+      
+      
+      
 
   
